bbc3b0703db89ff4ef08a9aa809a4db63d30efab
kiki <1152508446@qq.com>
2023-09-24 00:47:29 +0800

    update

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 # import aspose.pdf as ap
-
+import pdf2image
+import time
 # Create your views here.
 
 DIR_INPUT = "../upload_files/"
@@ -49,7 +50,25 @@ def index(request):
 #         device.process(document.pages[i + 1], imageStream)
 #         imageStream.close()
 
+def pdf2img():
+    # This method reads a pdf and converts it into a sequence of images
+    # PDF_PATH sets the path to the PDF file
+    # dpi parameter assists in adjusting the resolution of the image
+    # output_folder parameter sets the path to the folder to which the PIL images can be stored (optional)
+    # first_page parameter allows you to set a first page to be processed by pdftoppm
+    # last_page parameter allows you to set a last page to be processed by pdftoppm
+    # fmt parameter allows to set the format of pdftoppm conversion (PpmImageFile, TIFF)
+    # thread_count parameter allows you to set how many thread will be used for conversion.
+    # userpw parameter allows you to set a password to unlock the converted PDF
+    # use_cropbox parameter allows you to use the crop box instead of the media box when converting
+    # strict parameter allows you to catch pdftoppm syntax error with a custom type PDFSyntaxError
 
+    start_time = time.time()
+    pil_images = pdf2image.convert_from_path(PDF_PATH, dpi=DPI, output_folder=OUTPUT_FOLDER, first_page=FIRST_PAGE,
+                                             last_page=LAST_PAGE, fmt=FORMAT, thread_count=THREAD_COUNT, userpw=USERPWD,
+                                             use_cropbox=USE_CROPBOX, strict=STRICT)
+    print("Time taken : " + str(time.time() - start_time))
+    return pil_images
 
 
 class upload_files(View):
